3c59185b27155502db1bdb66269af0a749b718bc
elianalmanzar98-alt <elianalmanzar98@gmail.com>
2026-02-16 19:47:54 -0400

    Add files via upload

---
+++ problema1/Empleado.java
@@ -0,0 +1,10 @@
+public class Empleado {
+    private String nombre;
+    private double SalarioBase;
+
+
+    public Empleado(String nombre, double SalarioBase){
+        
+        nombre = nombre.isEmpty()
+    }
+}
